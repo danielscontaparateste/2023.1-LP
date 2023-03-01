c9c23edc8b46de3653e3cbf7d18317bbabfeabbb
Daniel Ferreira <danielse@ifce.edu.br>
2023-03-01 14:09:54 -0300

    Aula 01/03

--- lacos/e8.c
+++ lacos/e8.c
@@ -6,7 +6,7 @@ int main(){
     for (int i=0; i<10; i++){
         printf("%d\t:\t",i);
         for (int j=0; j<10; j++){
-            // O continue ignora todas as instruções do laço posterior.
+            // O continue ignora todas as instruções posteriores do laço.
             if (j==3) continue; 
             printf("%d ",j);
         }
